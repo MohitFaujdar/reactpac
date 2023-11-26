44f17da35b28dab6fecc7f54e497590c6c5e176e
mohit faujdar <mohitfaujdar150@gmail.com>
2023-11-27 03:21:00 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css"
 import { useRef, useState } from "react"
-import FirstComponents from "./components/firstComponents"; 
+import FirstComponents from "./components/FirstComponents"; 
 
 const App = () => {
   const [data, setData] = useState([])
